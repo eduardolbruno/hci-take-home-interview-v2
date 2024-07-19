70eb17f24033e86e894cd7bae5ef28b1253dd28d
Eduardo Bruno <eduardo.luis.bruno@gmail.com>
2024-07-19 16:00:45 +0100

    [API] Added new services

--- PatientAdministrationSystem.Application/Services/Interfaces/IPatientsService.cs
+++ PatientAdministrationSystem.Application/Services/Interfaces/IPatientsService.cs
@@ -1,3 +1,4 @@
+using PatientAdministrationSystem.Application.Dtos;
 using PatientAdministrationSystem.Application.Entities;
 
 namespace PatientAdministrationSystem.Application.Interfaces;
@@ -7,5 +8,7 @@ public interface IPatientsService
     // Define your service interface here for use in your API and service
 
     public Task<IEnumerable<PatientEntity>> GetAll();
-    public Task<IEnumerable<PatientEntity>> GetByName(string name);
+    public Task<IEnumerable<PatientEntity>> GetAllPatientsAsync();
+    public Task<IEnumerable<PatientDto>> GetAllPatientVisitsAsync(string search);
+
 }
--- PatientAdministrationSystem.Application/Services/PatientsService.cs
+++ PatientAdministrationSystem.Application/Services/PatientsService.cs
@@ -1,3 +1,4 @@
+using PatientAdministrationSystem.Application.Dtos;
 using PatientAdministrationSystem.Application.Entities;
 using PatientAdministrationSystem.Application.Interfaces;
 using PatientAdministrationSystem.Application.Repositories.Interfaces;
@@ -21,10 +22,43 @@ public class PatientsService : IPatientsService
         return _repository.GetAll();
     }
 
-    public Task<IEnumerable<PatientEntity>> GetByName(string name)
+    public Task<IEnumerable<PatientEntity>> GetAllPatientsAsync()
     {
-        return _repository.GetByName(name);
+        return _repository.GetAllPatientsAsync();
     }
 
+    public async Task<IEnumerable<PatientDto>> GetAllPatientVisitsAsync(string search)
+    {
+        var patientRelations = await _repository.GetAllPatientVisitsAsync(search);
+
+        var patientDtos = patientRelations.Select(ph => new PatientDto
+        {
+            Id = ph.Patient.Id,
+            FirstName = ph.Patient.FirstName,
+            LastName = ph.Patient.LastName,
+            Email = ph.Patient.Email,
+            PatientHospitals = new List<PatientHospitalDto>
+            {
+                new PatientHospitalDto
+                {
+                    PatientId = ph.PatientId,
+                    HospitalId = ph.HospitalId,
+                    VisitId = ph.VisitId,
+                    Hospital = new HospitalDto
+                    {
+                        Id = ph.Hospital.Id,
+                        Name = ph.Hospital.Name
+                    },
+                    Visit = new VisitDto
+                    {
+                        Id = ph.Visit.Id,
+                        Date = ph.Visit.Date
+                    }
+                }
+            }
+        });
+
+        return patientDtos;
+    }
 
 }
