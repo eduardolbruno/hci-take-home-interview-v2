eeb2d5cc9604bff6990fd19501f2088832dc65eb
Eduardo Bruno <eduardo.luis.bruno@gmail.com>
2024-07-19 18:17:42 +0100

    [FE] Updated PatientList

--- Client/ClientApp/src/components/PatientList.js
+++ Client/ClientApp/src/components/PatientList.js
@@ -1,34 +1,80 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import axiosInstance from '../api/axiosInstance';
 
-const PatientList = () => {
+const PatientSearch = () => {
+    const [searchTerm, setSearchTerm] = useState('');
     const [patients, setPatients] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
 
-    useEffect(() => {
-        const fetchPatients = async () => {
-            try {
-                const response = await axiosInstance.get('/patients');
-                setPatients(response.data);
-            } catch (error) {
-                console.error('Error fetching patients:', error);
-            }
-        };
+    const handleSearch = async (event) => {
+        event.preventDefault();
+        setLoading(true);
 
-        fetchPatients();
-    }, []);
+        try {
+            const response = await axiosInstance.get(`/patients/search/${searchTerm}`);
+            setPatients(response.data);
+        } catch (error) {
+            setError(error.message || 'An error occurred');
+        } finally {
+            setLoading(false);
+        }
+    };
 
     return (
         <div>
-            <h1>Patient List</h1>
-            <ul>
-                {patients.map(patient => (
-                    <li key={patient.id}>
-                        {patient.firstName} {patient.lastName}
-                    </li>
-                ))}
-            </ul>
+            <h1>Search Patients</h1>
+            <form onSubmit={handleSearch} className="search-form">
+                    <input
+                        className="search-input"
+                        type="text"
+                        value={searchTerm}
+                        onChange={(e) => setSearchTerm(e.target.value)}
+                        placeholder="Search patients by name"
+                    />
+                    <button type="submit" className="search-button">Search</button>
+            </form>
+
+            {loading && <p>Loading...</p>}
+            {error && <p>Error: {error}</p>}
+            {patients.length > 0 && (
+                <table>
+                    <thead>
+                        <tr>
+                            <th>First Name</th>
+                            <th>Last Name</th>
+                            <th>Email</th>
+                            <th>Hospital</th>
+                            <th>Visit Date</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {patients.map((patient) => (
+                            <tr key={patient.id}>
+                                <td>{patient.firstName}</td>
+                                <td>{patient.lastName}</td>
+                                <td>{patient.email}</td>
+                                <td>
+                                    {patient.patientHospitals.map((relation) => (
+                                        <div key={relation.hospitalId}>
+                                            {relation.hospital.name}
+                                        </div>
+                                    ))}
+                                </td>
+                                <td>
+                                    {patient.patientHospitals.map((relation) => (
+                                        <div key={relation.visitId}>
+                                            {new Date(relation.visit.date).toLocaleDateString()}
+                                        </div>
+                                    ))}
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            )}
         </div>
     );
 };
 
-export default PatientList;
+export default PatientSearch;
