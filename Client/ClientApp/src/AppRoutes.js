9a287689b6b4a74f0cb6b2d8a86946b88c7abf0a
Eduardo Bruno <eduardo.luis.bruno@gmail.com>
2024-07-19 18:17:10 +0100

    [FE] Removed unnecesary files from boilerplate

--- Client/ClientApp/src/AppRoutes.js
+++ Client/ClientApp/src/AppRoutes.js
@@ -1,20 +1,10 @@
-import { Counter } from "./components/Counter";
-import { FetchData } from "./components/FetchData";
 import { Home } from "./components/Home";
 import PatientList from "./components/PatientList";
 
 const AppRoutes = [
-  {
-    index: true,
-    element: <Home />
-  },
-  {
-    path: '/counter',
-    element: <Counter />
-  },
-  {
-    path: '/fetch-data',
-    element: <FetchData />
+    {
+        index: true,
+        element: <Home />
     },
     {
         path: '/search',
--- Client/ClientApp/src/components/NavMenu.js
+++ Client/ClientApp/src/components/NavMenu.js
@@ -25,22 +25,16 @@ export class NavMenu extends Component {
     return (
       <header>
         <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3" container light>
-          <NavbarBrand tag={Link} to="/">Client</NavbarBrand>
+                <NavbarBrand tag={Link} to="/">Patient Admission Service</NavbarBrand>
           <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
           <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={!this.state.collapsed} navbar>
             <ul className="navbar-nav flex-grow">
               <NavItem>
                 <NavLink tag={Link} className="text-dark" to="/">Home</NavLink>
               </NavItem>
-              <NavItem>
-                <NavLink tag={Link} className="text-dark" to="/counter">Counter</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink tag={Link} className="text-dark" to="/fetch-data">Fetch data</NavLink>
-                        </NavItem>
-                        <NavItem>
-                            <NavLink tag={Link} className="text-dark" to="/search">Search Patients</NavLink>
-                        </NavItem>
+                <NavItem>
+                    <NavLink tag={Link} className="text-dark" to="/search">Search Patients</NavLink>
+                </NavItem>
             </ul>
           </Collapse>
         </Navbar>
